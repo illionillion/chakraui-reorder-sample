f1371ee6ba7afd6db0ea17a408119cf3bbfd59a2
yuto ichinose <yuto.0526.sano@gmail.com>
2023-03-15 02:46:43 +0900

    成功例のサンプル完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Center, List, ListItem, Text } from "@chakra-ui/react";
+import { Reorder, useDragControls } from "framer-motion";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [usernames, setUsernames] = useState([
+    "malerba118",
+    "compulves",
+    "dan_abramov",
+    // {name:"malerba118"}, // オブジェクトはだめ
+    // {name:"compulves"},
+    // {name:"dan_abramov"},
+  ]);
+  const controls = useDragControls();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Center h="100vh">
+      <List
+        as={Reorder.Group}
+        axis="y"
+        values={usernames}
+        onReorder={setUsernames}
+        spacing={2}
+      >
+        {usernames.map((item, index) => (
+          <ListItem
+            as={Reorder.Item}
+            key={item} // key と itemが一緒？
+            value={item}
+            p={2}
+            bg="gray.100"
+            rounded="xl"
+            dragListener={false}
+            draggable={false}
+            dragControls={controls}
+          >
+            <Text onPointerDown={(e) => controls.start(e)} cursor="grab">
+              {item}
+            </Text>
+          </ListItem>
+        ))}
+      </List>
+    </Center>
   );
 }
 
