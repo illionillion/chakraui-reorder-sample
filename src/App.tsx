41b3e7c13ebdd73ea34d4514a567ea7b302e7493
yuto ichinose <yuto.0526.sano@gmail.com>
2023-03-21 17:49:01 +0900

    モバイル時の対応追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import { Center, List } from "@chakra-ui/react";
 import { Reorder } from "framer-motion";
 import { useState } from "react";
 import "./App.css";
-import { Item, Item2 } from "./Item";
+import { Item } from "./Item";
 
 function App() {
   const [usernames, setUsernames] = useState([
     "malerba118",
     "compulves",
     "dan_abramov",
-    // {name:"malerba118"}, // オブジェクトはだめ？
-    // {name:"compulves"},
-    // {name:"dan_abramov"},
   ]);
 
   return (
@@ -27,11 +24,6 @@ function App() {
           <Item key={item} item={item} />
         ))}
       </List>
-      {/* <Reorder.Group axis="y" onReorder={setUsernames} values={usernames}>
-        {usernames.map((item) => (
-          <Item2 key={item} item={item} />
-        ))}
-      </Reorder.Group> */}
     </Center>
   );
 }
--- src/Item.tsx
+++ src/Item.tsx
@@ -1,10 +1,30 @@
 import { ListItem, Text, useBoolean } from "@chakra-ui/react";
 import { Reorder, useDragControls } from "framer-motion";
-import { FC } from "react";
+import { FC, LegacyRef, TouchEventHandler, useEffect, useRef } from "react";
 
 export const Item: FC<{ item: string }> = ({ item }) => {
   const controls = useDragControls();
-  const [isDragListener, setIsDragListener] = useBoolean(false);
+
+  // モバイル対応
+  const iRef = useRef<LegacyRef<SVGSVGElement> | undefined>(null);
+  useEffect(() => {
+    const touchHandler: TouchEventHandler<HTMLElement> = (e) =>
+      e.preventDefault();
+
+    const iTag = iRef.current;
+
+    if (iTag) {
+      //@ts-ignore
+      iTag.addEventListener("touchstart", touchHandler, { passive: false });
+
+      return () => {
+        //@ts-ignore
+        iTag.removeEventListener("touchstart", touchHandler, {
+          passive: false,
+        });
+      };
+    }
+  }, [iRef]);
 
   return (
     <ListItem
@@ -14,43 +34,19 @@ export const Item: FC<{ item: string }> = ({ item }) => {
       p={2}
       bg="gray.100"
       rounded="xl"
-      dragListener={isDragListener}
-      draggable={isDragListener}
+      dragListener={false}
       dragControls={controls}
     >
       <Text
+        //@ts-ignore
+        ref={iRef}
         onPointerDown={(e) => {
-          setIsDragListener.on();
           controls.start(e);
-        }}
-        onPointerUp={(e) => {
-          setIsDragListener.off();
         }}
         cursor="grab"
       >
         {item}
       </Text>
     </ListItem>
-  );
-};
-
-export const Item2: FC<{ item: string }> = ({ item }) => {
-  const controls = useDragControls();
-
-  return (
-    <Reorder.Item
-      key={item} // key と itemが一緒？
-      value={item}
-      style={{
-        padding: 2,
-        background: "gray",
-      }}
-      dragListener={false}
-      dragControls={controls}
-    >
-      <Text onPointerDown={(e) => controls.start(e)} cursor="grab">
-        {item}
-      </Text>
-    </Reorder.Item>
   );
 };
